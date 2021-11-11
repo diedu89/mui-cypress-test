860710d15c2bd6a8876985cb27c2cece3553d938
Diego Melendez <diedu89@gmail.com>
2021-11-10 20:56:00 -0600

    minimal reproduction

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,12 +1,13 @@
 // type definitions for Cypress object "cy"
 /// <reference types="cypress" />
 
-describe("App", function() {
-  beforeEach(function() {
+describe("App", function () {
+  beforeEach(function () {
     cy.visit("/");
   });
 
-  it("renders the app", function() {
-    cy.get(".App-link").should("contain", "Learn React");
+  it("renders the app", function () {
+    const date = "11102021";
+    cy.get("input.MuiInputBase-input").clear().type(date);
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import DateAdapter from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DatePicker from '@mui/lab/DatePicker';
+import TextField from '@mui/material/TextField';
 
 function App() {
+  const [value, setValue] = React.useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LocalizationProvider dateAdapter={DateAdapter}>
+        <DatePicker
+          id="test-dp"
+          label="Basic example"
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField id="test-dp" {...params} />}
+        />
+      </LocalizationProvider>
     </div>
   );
 }
